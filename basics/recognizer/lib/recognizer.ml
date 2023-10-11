1921d86078445adc06499fbda859f0537c81b8cb
AlbertoCorrias2 <a.corrias41@studenti.unica.it>
2023-10-11 15:44:52 +0200

    esercizi

--- basics/recognizer/lib/recognizer.ml
+++ basics/recognizer/lib/recognizer.ml
@@ -1,14 +1,28 @@
-let lang1 _ = failwith ""
+let binaryDigits s = List.filter(fun x -> x='0' || x='1') s = s;;
 
-let lang2 _ = failwith ""
+let lang1 s = binaryDigits s && s<>[];;
 
-let lang3 _ = failwith ""
+let lang2 s = binaryDigits s && match s with
+	[] -> false
+	| _::tl -> List.filter(fun x -> x='1') tl = tl;;
 
-let lang4 _ = failwith ""
+let lang3 s = binaryDigits s && match s with
+	[] -> false
+|	'0'::tl -> (match List.rev tl with
+		'0'::_ -> true
+	|	_ -> false)
+|	_ -> false;;
 
-let lang5 _ = failwith ""
+let lang4 s = binaryDigits s && List.length(List.filter(fun x -> x='1') s) = 2;; 
+
+let rec couple s = match s with
+	[] -> true
+|	el::el2::tl -> if(el <> el2) then false else couple tl
+|	_ -> false;;
+
+let lang5 s = binaryDigits s && List.length s >= 2 && couple s;;
+	
     
 let recognizers = [lang1;lang2;lang3;lang4;lang5]
                   
 let belongsTo w = List.map (fun f -> f w) recognizers
-  
--- basics/tugofwar/lib/tugofwar.ml
+++ basics/tugofwar/lib/tugofwar.ml
@@ -1,21 +1,39 @@
 (* tokens *)
 type token = A | B | X
 
+let explode s =
+  let rec exp i l =
+    if i < 0 then l else exp (i - 1) (s.[i] :: l) in
+  exp (String.length s - 1) []
+
 (* val toklist_of_string : string -> token list *)
 (* toklist_of_string s transforms the string s into a list of tokens *)
 (* Hint: use the function explode in bin/main.ml to convert a string to a char list *)
              
-let toklist_of_string s = failwith "TODO"
+let toklist_of_string s = explode s;;
+	
 
 (* val valid : token list -> bool *)
 (* valid l is true when l is a list of tokens in the language A* X* B* *)
     
-let valid l = failwith "TODO"
+let rec valid l = match l with
+	[] -> false
+	| 'A'::'='::tl -> if(List.filter(fun x -> x = 'A') tl = []) then valid tl else false
+	| '='::'B'::tl -> List.filter(fun x -> x = '=' || x = 'A') tl = []
+	| '='::tl -> List.filter(fun x -> x = 'A') tl = []
+	| 'B'::tl -> List.filter(fun x -> x = '=' || x = 'A') tl = []
+	| _::tl -> valid tl;;
 
 (* val win : token list -> token *)
 (* win l determines the winner of a tug of war game. X means tie *)
 
-let win l = failwith "TODO"
+let rec count l tok = match l with
+	[] -> 0
+	| hd::tl when hd = tok -> 1 + count tl tok
+	| _::tl -> 0 + count tl tok;;
+
+
+let win l = if(count l 'A' > count l 'B') then A else if(count l 'A' = count l 'B') then X else B;;
 
 (* val string_of_winner : token -> string *)
-let string_of_winner w = failwith "TODO"
+let string_of_winner w = if (w = X) then "It's a draw" else if w = A then "The winner of the tug of war is A" else "The winner of the tug of war is B";;
